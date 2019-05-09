0b8be07a520963ba990e8dd80dd04c7cdc137628
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-05-09 07:55:30 -0500

    use functional component in app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,4 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/Homepage';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Homepage />
-      </div>
-    );
-  }
-}
+export const App = () => <Homepage />;
