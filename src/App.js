791feee2a509c5dfb32f1377f6c2ec0360036dba
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-05-11 10:18:40 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Homepage from './components/pages/Homepage';
 import Single from './components/pages/Single';
+import NavMenu from './components/layout/NavMenu';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export const App = () => (
   <Router>
+    <NavMenu />
     <Switch>
       <Route exact path="/" component={Homepage} />
       <Route exact path="/post/:id" component={Single} />
---
+++ src/components/layout/NavMenu.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+const GET_MENU_ITEMS = gql`
+  query {
+    menu(id: "TWVudToyOA==") {
+      menuItems {
+        nodes {
+          label
+          url
+        }
+      }
+    }
+  }
+`;
+
+class NavMenu extends Component {
+  render() {
+    const { loading, menu } = this.props.data;
+    let menuItems;
+
+    if (loading) {
+      menuItems = [];
+    } else {
+      menuItems = menu.menuItems.nodes;
+      console.log('data', menuItems);
+    }
+    return (
+      <div className="main-menu">
+        <div className="row align-items-center justify-content-center d-flex">
+          <nav id="nav-menu-container">
+            <ul className="nav-menu">
+              {menuItems.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <a href={item.url}>{item.label}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default graphql(GET_MENU_ITEMS)(NavMenu);
