1129c36d54d823484695b17eb540fd5acb9a4e35
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-05-15 05:37:58 -0500

    hide logo - fix single blog styles

--- src/components/layout/header/Logo.js
+++ src/components/layout/header/Logo.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from '../../../img/logo-blackbird.png';
+//import logo from '../../../img/logo-blackbird.png';
 
 const Logo = () => {
   return (
-    <a href="/">
-      <img className="main-logo" src={logo} alt="logo" />;
-    </a>
+    <a href="/">{/* <img className="main-logo" src={logo} alt="logo" /> */}</a>
   );
 };
 
