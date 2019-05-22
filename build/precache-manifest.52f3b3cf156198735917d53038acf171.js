2a05d6a5299f8304060d57b98f19d99b17ebb9cb
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-05-22 11:43:06 -0500

    build folder for Heorku

---
+++ build/precache-manifest.52f3b3cf156198735917d53038acf171.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f5587a7bf5a97cf1a7e2690327cc3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc3b2422c6854a7fd6f9",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d1d4814e5d66911cae3e",
+    "url": "/static/css/main.10f427af.chunk.css"
+  },
+  {
+    "revision": "dc3b2422c6854a7fd6f9",
+    "url": "/static/js/2.096106d3.chunk.js"
+  },
+  {
+    "revision": "d1d4814e5d66911cae3e",
+    "url": "/static/js/main.f1a3730d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b9b7f23cb61b1f503e1249b63d980448",
+    "url": "/static/media/Linearicons-Free.b9b7f23c.eot"
+  },
+  {
+    "revision": "02e4b2d5c7b6c89d3784e6c814780e3a",
+    "url": "/static/media/banner-bg.02e4b2d5.jpg"
+  }
+]);
